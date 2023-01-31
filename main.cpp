8bbfdd2ef576fee7729f34456dd007c6b19b73d4
AdonisCreed3 <123179420+AdonisCreed3@users.noreply.github.com>
2023-01-31 11:56:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,111 @@
-#include <iostream>
-
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
+#include "operatii.h"
 using namespace std;
 #define LAT 24
 #define LUNG 12
-int zar(){
-    int nr = rand();
-    while(!nr){
-        nr = rand();
-    }
-    return nr;
-        
-}
 
-int main()
-{
-    
-    
+
+int main(){
+    srand(time(NULL));
+    system("chcp 65001 > nul");
+    nod *p = new nod;
+    p->culoare=0;
+    p->info=5;
+    nod*q = new nod;
+    q=p;
+    for(int i = 1; i <= 4; i++){
+        nod*n = new nod;
+        q->leg=n;
+        n->info=0;
+        n->culoare=0;
+        q=n;
+    }
+
+    nod*l=new nod;
+    q->leg=l;
+    l->info=2;
+    l->culoare=1;
+    q=l;
+
+    l=new nod;
+    l->info = 5;
+    l->culoare=1;
+    q->leg=l;
+    q=l;
+
+    for(int i = 1; i <= 3; i++){
+        nod*n = new nod;
+        q->leg=n;
+        n->info=0;
+        n->culoare=0;
+        q=n;
+    }
+
+    l=new nod;
+    l->info=3;
+    l->culoare=0;
+    q->leg=l;
+    q=l;
+
+    l=new nod;
+    l->info=0;
+    l->culoare=0;
+    q->leg=l;
+    q=l;
+
+    l = new nod;
+    l->info=5;
+    l->culoare=1;
+    q->leg=l;
+    q=l;
+
+     for(int i = 1; i <= 4; i++){
+        nod *n = new nod;
+        q->leg=n;
+        n->info=0;
+        n->culoare=0;
+        q=n;
+    }
+    l=new nod;
+    l->info=2;
+    l->culoare=0;
+    q->leg=l;
+    q=l;
+
+    l=new nod ;
+    l->info =5;
+    l->culoare=0;
+    q->leg=l;
+    q=l;
+
+    for(int i = 1; i <= 3; i++){
+        nod *n = new nod;
+        q->leg=n;
+        n->info=0;
+        n->culoare=0;
+        q=n;
+    }
+
+    l=new nod;
+    l->info=3;
+    l->culoare=1;
+    q->leg=l;
+    q=l;
+
+    l=new nod;
+    l->info=0;
+    l->culoare=0;
+    q->leg=0;
+    q=l;
+
+
+
+
+
+
+
     for(int i = 1; i <= LUNG; i++){
         int lat = 1;
         while(lat!=25){
@@ -23,19 +113,18 @@ int main()
                 cout << " ";
             }else if(lat == 24 || lat == 1 || lat == 14 || lat ==13)
                 cout << '|';
-            else if(i < 6){ 
-                if(lung-i+1 <= cauta(lat/2+12)){
-                    piesa(culoare(lat/2));
+            else if(i < 6){
+                if(LUNG-i+1 <= cauta(lat/2+12,p)){
+                    piesa(culoare(lat/2,p));
                 }else{
                     cout << " ";
                 }
             }else{
-                
+
             }
         }
         cout << endl;
     }
-    
 
     return 0;
 }
