e8384117c1393154337f70e4d73da07665fab569
vic547 <123238001+vic547@users.noreply.github.com>
2023-02-03 12:01:42 +0200

    Update operatii.h

--- operatii.h
+++ operatii.h
@@ -1,7 +1,9 @@
 #include<stdlib.h>
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
+
 
 
 
@@ -11,23 +13,38 @@ struct nod{
     nod*leg;
 };
 
+bool piesa2(){
+    int xps=xpi+13, xps=yps=ypi+4;
+}
+
+bool piesa(int x,int y,nod*p)
+{
+    for(int i=0;i<120;i++)
+    {
+        if(piesa2(piese[i].xpi , piese[i].ypi))
+        {
+            return true;
+        }
+    }
+}
+
 bool culoare(int n,nod*p){
-		int ord = 1;
+    nod*q;
+    int ord = 1;
     while(ord != n)
     {
         p = p->leg;
-				ord++;
     }
     return p->culoare;
 }
 
 int cauta(int n,nod*p)
 {
-    int ord = 1;
+    nod*q,*u;
+    int ord;
     while(ord!=n)
     {
         p=p->leg;
-				ord++;
     }
     return p->info;
 }
